ecd6e1a70e25d9f914a395677662a69233778bfc
saul422 <139730851+saul422@users.noreply.github.com>
2024-10-26 22:42:16 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,6 +19,10 @@ int main() {
     srand(time(0));
     bool again;
 
+    list<Goat> trip; //list that holds goat objects
+
+
+
     // read & populate arrays for names and colors
     ifstream fin("names.txt");
     string names[SZ_NAMES];
@@ -32,7 +36,28 @@ int main() {
     fin1.close();
 
 //call main_menu function
+bool quit = false;
+while (!quit) {
+    int choice = main_menu();
+    switch(choice){
+        case 1:
+            add_goat(trip,names,colors);
+            break;
+        case 2:
+            delete_goat(trip);
+            break;
+        case 3:
+            display_trip(trip);
+            break;
+        case 4:
+            quit = true;
+            cout << " Exiting Goat Manager 3001";
+            break;
+        default:
+            cout<<"Invalid choice, please try agian. \n";
+    }
 
+}
 
     return 0;
 }
@@ -53,10 +78,65 @@ return choice;
 };
 
 
+void add_goat(list<Goat> &trip, const string names[], const string colors [])
+{
+    string name = names [rand() % SZ_NAMES];
+    int age = rand() % ( MAX_AGE + 1 );
+    string color = colors [rand() % SZ_COLORS];
+
+    Goat new_goat(name, age, color);
+    trip.push_back(new_goat);
+
+    cout<<"Added goat: ";
+    new_goat.display();
+    cout << endl;
 
 
+}
 
+//delete selecte goat
+void delete_goat(list<Goat> &trip) {
+        if (trip.empty()) {
+            cout << "No goats to delete. \n";
+            return;
+        }
 
+        int index = select_goat(trip);
+        if (index == -1) return;
+
+        auto it = trip.begin();
+        advance(it, index);
+        cout<< "Deleting goat: ";
+        it -> display();
+        trip.erase(it);
+}
+//display all goats in list
+void display_trip(const list<Goat> &trip ) {
+    if (trip.empty()) {
+         cout << "No goats to delete. \n";
+            return;
+    }
+    int i = 1;
+    for (const Goat &g : trip) {
+        cout<< "[ "<< i++ << "]";
+        g.display();
+        cout<< endl;
+    }
+}
+//select goat by index
+int select_goat(const list<Goat> &trip) {
+    display_trip(trip);
+    int choice;
+    cout<< "Select a goat by a number (or 0 to cancel): ";
+    cin >> choice;
+
+    if (choice < 1 || choice > trip.size()) {
+        cout<< " Canceled or invalid choice. \n";
+        return -1;
+    }
+    return choice - 1;
+
+}
 
 
 
